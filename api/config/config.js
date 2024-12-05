4aba47d0b8dd3dfb3f11edb8b2e6b6db2386669f
VladimirRudenk0 <131373045+VladimirRudenk0@users.noreply.github.com>
2024-12-05 14:53:54 +0300

    Refactor: replace config.json with config.js & integrate dotenv

---
+++ api/config/config.js
@@ -0,0 +1,29 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    dialect: 'postgres',
+  },
+  test: {
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    dialect: 'postgres',
+  },
+  production: {
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    dialect: 'postgres',
+  },
+};
